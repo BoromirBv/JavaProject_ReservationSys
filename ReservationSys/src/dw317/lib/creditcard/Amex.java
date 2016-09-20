5968ea79d88c10b7706218fa5b4f4189ce5c809f
Andy <andreeaid@users.noreply.github.com>
2016-09-19 20:44:11 -0400

    Add files via upload

--- ReservationSys/src/dw317/lib/creditcard/Amex.java
+++ ReservationSys/src/dw317/lib/creditcard/Amex.java
@@ -1,13 +1,59 @@
 package dw317.lib.creditcard;
 
+/**
+ * @author Jephthia Louis, Andreea Draghicescu
+ *
+ */
+
 public class Amex extends AbstractCreditCard {
 	private static final long serialVersionUID = 42031768871L;
 
+	/*
+	 * @author Jephthia Louis
+	 *
+	 */
 	public Amex(String number) {
 		super(CardType.AMEX, validateNumber(number));
 	}
 
+	/*
+	 * @author Andreea Draghicescu
+	 * 
+	 * @return the number if it is 15 digits long and the first two digits are
+	 * 34 or 37.
+	 */
 	private static String validateNumber(String number) throws IllegalArgumentException {
-		return null;
+		// Test if the number is null and throws and exception if true.
+		if (number != null) {
+			if (number.isEmpty()) { // empty
+				throw new IllegalArgumentException
+					("Credit card Amex error: " + number + "is empty.");
+			}
+
+			// See if the number has anything else than numbers.
+			if (!number.matches("[0-9]+")) {
+				throw new IllegalArgumentException
+					("Credit card Amex error: " + number + " has an invalid character.");
+			}
+
+			if (number.length() != 15) { // length
+				throw new IllegalArgumentException
+					("Credit card Amex error: " + number + " is not 15 digits long.");
+			}
+
+			// Converts the two first digits to an integer
+			int startingDigits = Integer.parseInt
+						(number.substring(0, 2));
+
+			if (startingDigits == 34 || startingDigits == 37) {
+				return number;
+			} else {
+				throw new IllegalArgumentException
+					("Credit card Amex error: " + number + " first two digits are invalid.");
+			}
+		} else {
+			throw new NullPointerException
+				("Credit card Amex error: is " + number);
+		}
 	}
 }
