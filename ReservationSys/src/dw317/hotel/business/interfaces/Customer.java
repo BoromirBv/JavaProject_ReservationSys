c1d627d024986292f5bd7ede6470e32692681530
Andy <andreeaid@users.noreply.github.com>
2016-09-21 21:43:08 -0400

    update

--- ReservationSys/src/dw317/hotel/business/interfaces/Customer.java
+++ ReservationSys/src/dw317/hotel/business/interfaces/Customer.java
@@ -2,25 +2,21 @@ package dw317.hotel.business.interfaces;
 
 import java.io.Serializable;
 import java.util.Optional;
-
 import dw317.lib.Email;
 import dw317.lib.Name;
+import dw317.lib.creditcard.CreditCard;
+
+/*
+ * @author Nicolas Fontaine
+ */
 
 public interface Customer extends Comparable<Customer>, Serializable{
 	
-	public Name getName(){
-		
-	}
+	public Name getName();
 	
-	public Email getEmail(){
-		
-	}
+	public Email getEmail();
 	
-	public Optional<CreditCard> getCreditCard(){
-		
-	}
+	public Optional<CreditCard> getCreditCard();
 	
-	public void setCreditCard(Optional<CreditCard> card){
-		
-	}
+	public void setCreditCard(Optional<CreditCard> card);
 }
--- ReservationSys/src/dw317/hotel/business/interfaces/Room.java
+++ ReservationSys/src/dw317/hotel/business/interfaces/Room.java
@@ -4,14 +4,16 @@ import java.io.Serializable;
 
 import dw317.hotel.business.RoomType;
 
+/* @author Nicolas Fontaine
+ * 
+ */
+
 public interface Room extends Comparable<Room>, Serializable {
-	public RoomType getRoomType(){
-	}
+	public RoomType getRoomType();
+	
+	public int getFloor();
 
-	public int getRoomNumber(){
-	}
+	public int getRoomNumber();
 
-	public int getNumber(){
-		
-	}
+	public int getNumber();
 }
