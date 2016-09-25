664b56524c9da9a371116dcd7d5fcc575ce891d9
Andy <andreeaid@users.noreply.github.com>
2016-09-25 00:48:35 -0400

    Update: 25/09/2016
    Added all necessary comments for complete methods and classes.

--- ReservationSys/test/dw317/lib/creditcard/MasterCardTest.java
+++ ReservationSys/test/dw317/lib/creditcard/MasterCardTest.java
@@ -6,59 +6,56 @@ package dw317.lib.creditcard;
  */
 
 public class MasterCardTest {
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		testTheOneParameterConstructor();
+		testMasterCardConstructor();
 	}
-	
-	private static void testTheOneParameterConstructor() {
-		testTheOneParameterConstructor(
-				"Case 1 - Valid data – valid number (16 digits long and 51-55 range)",
-						"5333802494710623", true);
-		testTheOneParameterConstructor(
-				"Case 2 - Invalid data – invalid first two digits (not in the 51-55 range)",
-						"6033802494710623", false);
-		testTheOneParameterConstructor(
-				"Case 3 - Invalid data – number too short","53338024947106", false);
-		testTheOneParameterConstructor(
-				"Case 4 - Invalid data – number too long","53338024947106536", false);
-		testTheOneParameterConstructor(
-				"Case 5 - Invalid data – empty number","", false);
-		testTheOneParameterConstructor(
-				"Case 6 - Invalid data – invalid character or characters","53f38024947106536", false);
-		testTheOneParameterConstructor(
-				"Case 7 - Invalid data – null number", null, false);
-		
+
+	/* @author Andreea Draghicescu 
+	 * Tests the credit card number for different cases. The boolean represent 
+	 * what we expect the case to return. If it returns the opposite, the test has failed.
+	 */
+	private static void testMasterCardConstructor() {
+		testMasterCardConstructor("Case 1 - Valid data – valid number (16 digits long and 51-55 range)",
+				"5333802494710623", true);
+		testMasterCardConstructor("Case 2 - Invalid data – invalid first two digits (not in the 51-55 range)",
+				"6033802494710623", false);
+		testMasterCardConstructor("Case 3 - Invalid data – number too short", "53338024947106", false);
+		testMasterCardConstructor("Case 4 - Invalid data – number too long", "53338024947106536", false);
+		testMasterCardConstructor("Case 5 - Invalid data – empty number", "", false);
+		testMasterCardConstructor("Case 6 - Invalid data – invalid character or characters", "53f38024947106536",
+				false);
+		testMasterCardConstructor("Case 7 - Invalid data – null number", null, false);
+
 	}
-	
-	private static void testTheOneParameterConstructor(String testCase, String number,
-			boolean expectValid) {
+
+	/* @author Andreea Draghicescu.
+	 * @param testCase, number, expectValid 
+	 * Creates an instance of MasterCard if it is a valid credit card and the 
+	 * case are successful or not.
+	 */
+	private static void testMasterCardConstructor(String testCase, String number, boolean expectValid) {
 
 		System.out.println("   " + testCase);
 
 		try {
 			MasterCard mastercard = new MasterCard(number);
 			System.out.print("\tThe MasterCard instance was created: " + mastercard);
-			
+
 			if (!expectValid)
 				System.out.print("Error! Expected Invalid. ==== FAILED TEST ====");
-		}
-		catch (IllegalArgumentException iae)	{
-			System.out.print("\t"+ iae.getMessage());
+		} catch (IllegalArgumentException iae) {
+			System.out.print("\t" + iae.getMessage());
+			if (expectValid)
+				System.out.print("Error! Expected Valid. ==== FAILED TEST ====");
+		} catch (NullPointerException npe) {
+			System.out.print("\t " + npe.getMessage());
 			if (expectValid)
 				System.out.print("Error! Expected Valid. ==== FAILED TEST ====");
 		}
-		catch (NullPointerException npe)	{
-			System.out.print("\t "+ npe.getMessage());
-			if (expectValid)
-				System.out.print("Error! Expected Valid. ==== FAILED TEST ====");
-		}
-		
+
 		catch (Exception e) {
-			System.out.print("\tUNEXPECTED EXCEPTION TYPE! " 
-					+ e.getClass() + " " + e.getMessage() + " ==== FAILED TEST ====");
+			System.out.print(
+					"\tUNEXPECTED EXCEPTION TYPE! " + e.getClass() + " " + e.getMessage() + " ==== FAILED TEST ====");
 			if (expectValid)
 				System.out.print("Expected Valid.");
 		}
--- ReservationSys/test/dw317/lib/creditcard/VisaTest.java
+++ ReservationSys/test/dw317/lib/creditcard/VisaTest.java
@@ -6,59 +6,56 @@ package dw317.lib.creditcard;
  */
 
 public class VisaTest {
-	/**
-	 * @param args
+	public static void main(String[] args) {
+		testVisaConstructor();
+	}
+
+	/* @author Andreea Draghicescu 
+	 * Tests the credit card number for different cases. The boolean represent 
+	 * what we expect the case to return. If it returns the opposite, the test has failed.
 	 */
-	public static void main(String[] args) {	
-		testTheOneParameterConstructor();		
+	private static void testVisaConstructor() {
+		testVisaConstructor("Case 1 - Valid data – valid number (16 digits long and 4 as first digit)",
+				"4532225872064086", true);
+		testVisaConstructor("Case 2 - Invalid data – invalid first digit (not equal to 4)",
+				"5333802494710623", false);
+		testVisaConstructor("Case 3 - Invalid data – number too short", "453222587206", false);
+		testVisaConstructor("Case 4 - Invalid data – number too long", "45322258720640863567", false);
+		testVisaConstructor("Case 5 - Invalid data – empty number", "", false);
+		testVisaConstructor("Case 6 - Invalid data – invalid character or characters", "453s2258h2064086",
+				false);
+		testVisaConstructor("Case 7 - Invalid data – null number", null, false);
+
 	}
-	
-	private static void testTheOneParameterConstructor() {
-		testTheOneParameterConstructor(
-				"Case 1 - Valid data – valid number (16 digits long and 4 as first digit)",
-						"4532225872064086", true);
-		testTheOneParameterConstructor(
-				"Case 2 - Invalid data – invalid first digit (not equal to 4)",
-						"5333802494710623", false);
-		testTheOneParameterConstructor(
-				"Case 3 - Invalid data – number too short","453222587206", false);
-		testTheOneParameterConstructor(
-				"Case 4 - Invalid data – number too long","45322258720640863567", false);
-		testTheOneParameterConstructor(
-				"Case 5 - Invalid data – empty number","", false);
-		testTheOneParameterConstructor(
-				"Case 6 - Invalid data – invalid character or characters","453s2258h2064086", false);
-		testTheOneParameterConstructor(
-				"Case 7 - Invalid data – null number", null, false);
-		
-	}
-	
-	private static void testTheOneParameterConstructor(String testCase, String number,
-			boolean expectValid) {
+
+	/* @author Andreea Draghicescu.
+	 * @param testCase, number, expectValid
+	 * Creates an instance of Visa if it is a valid credit card and the 
+	 * case are successful or not.
+	 */
+	private static void testVisaConstructor(String testCase, String number, boolean expectValid) {
 
 		System.out.println("   " + testCase);
 
 		try {
 			Visa visa = new Visa(number);
 			System.out.print("\tThe Visa instance was created: " + visa);
-			
+
 			if (!expectValid)
 				System.out.print("Error! Expected Invalid. ==== FAILED TEST ====");
-		}
-		catch (IllegalArgumentException iae)	{
-			System.out.print("\t"+ iae.getMessage());
+		} catch (IllegalArgumentException iae) {
+			System.out.print("\t" + iae.getMessage());
+			if (expectValid)
+				System.out.print("Error! Expected Valid. ==== FAILED TEST ====");
+		} catch (NullPointerException npe) {
+			System.out.print("\t " + npe.getMessage());
 			if (expectValid)
 				System.out.print("Error! Expected Valid. ==== FAILED TEST ====");
 		}
-		catch (NullPointerException npe)	{
-			System.out.print("\t "+ npe.getMessage());
-			if (expectValid)
-				System.out.print("Error! Expected Valid. ==== FAILED TEST ====");
-		}
-		
+
 		catch (Exception e) {
-			System.out.print("\tUNEXPECTED EXCEPTION TYPE! " 
-					+ e.getClass() + " " + e.getMessage() + " ==== FAILED TEST ====");
+			System.out.print(
+					"\tUNEXPECTED EXCEPTION TYPE! " + e.getClass() + " " + e.getMessage() + " ==== FAILED TEST ====");
 			if (expectValid)
 				System.out.print("Expected Valid.");
 		}
