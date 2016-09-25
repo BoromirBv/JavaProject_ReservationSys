b4c7b3ccae7dbd202cbd77f0e77dbe1c5316fbbe
Andy <andreeaid@users.noreply.github.com>
2016-09-25 00:47:37 -0400

    Update: 25/09/2016
    Added all necessary comments for complete methods and classes.

--- ReservationSys/src/group1/hotel/business/DawsonCustomer.java
+++ ReservationSys/src/group1/hotel/business/DawsonCustomer.java
@@ -5,74 +5,101 @@ import dw317.hotel.business.interfaces.Customer;
 import dw317.lib.*;
 import dw317.lib.creditcard.CreditCard;
 
-/*
+/**
  * @author Jephthia Louis
  *
  */
 
-public class DawsonCustomer implements Customer
-{
-    private static final long serialVersionUID = 42031768871L;
-    private final Name name;
-    private final Email email;
-    private final Optional<CreditCard> creditCard;
-    
-    public DawsonCustomer(String firstName, String lastName, String email)
-    {
-        name = new Name(firstName, lastName);
-        this.email = new Email(email);
-        creditCard = null;
-    }
-    @Override public int compareTo(Customer customer)
-    {
-        return this.getEmail().compareTo(customer.getEmail());
-    }
-    @Override public Name getName()
-    {
-        return new Name(name.getFirstName(), name.getLastName());
-    }
-    @Override public Email getEmail()
-    {
-        return email;
-    }
-    @Override public Optional<CreditCard> getCreditCard()
-    {
-        return null;
-    }
-    @Override public void setCreditCard(Optional<CreditCard> card)
-    {
-    }
-    @Override public boolean equals(Object object)
-    {
-        if(object == null)
-            return false;
-        
-        if(this == object)
-            return true;
-        
-        if(object instanceof DawsonCustomer)
-        {
-            DawsonCustomer customer = (DawsonCustomer)object;
-            
-            if(getEmail().equals(customer.getEmail()))
-                return true;
-        }
-        
-        return false;
-    }
-    @Override public int hashCode()
-    {
-        //TODO
-        return 0;
-    }
-    @Override public String toString()
-    {
-        String card = "*";
-        
-        if(creditCard.isPresent())
-            card = creditCard.get().getType().toString()+"*"+creditCard.get().getNumber();
-        
-        return getEmail().getAddress()+"*"+getName().getFirstName()+"*"+getName().getLastName()
-                +"*"+card;
-    }
+public class DawsonCustomer implements Customer {
+	private static final long serialVersionUID = 42031768871L;
+	private final Name name;
+	private final Email email;
+	private final Optional<CreditCard> creditCard;
+
+	public DawsonCustomer(String firstName, String lastName, String email) {
+		this.name = new Name(firstName, lastName);
+		this.email = new Email(email);
+		creditCard = null;
+	}
+	
+	/* @author Jephthia Louis
+	 * @return int
+	 * @param customer
+	 */
+	@Override
+	public int compareTo(Customer customer) {
+		return this.getEmail().compareTo(customer.getEmail());
+	}
+	
+	/* @author Jephthia Louis
+	 * @return boolean if the objects are equal or not. Two Customer objects are 
+	 * considered equal if they are instances of the same class and their 
+	 * email attributes are equal.
+	 * @param object
+	 */
+	@Override
+	public boolean equals(Object object) {
+		if (object == null)
+			return false;
+
+		if (this == object)
+			return true;
+
+		if (object instanceof DawsonCustomer) {
+			DawsonCustomer customer = (DawsonCustomer) object;
+
+			if (getEmail().equals(customer.getEmail()))
+				return true;
+		}
+
+		return false;
+	}
+	
+	/* @author Jephthia Louis
+	 * @return the credit card.
+	 */
+	@Override
+	public Optional<CreditCard> getCreditCard() {
+		return null;
+	}
+	
+	/* @author Jephthia Louis
+	 * @return the email address.
+	 */
+	@Override
+	public Email getEmail() {
+		return email;
+	}
+	
+	/* @author Jephthia Louis
+	 * @return the name.
+	 */
+	@Override
+	public Name getName() {
+		return new Name(name.getFirstName(), name.getLastName());
+	}
+
+	@Override
+	public int hashCode() {
+		return 0;
+	}
+	
+	/* @author Jephthia Louis
+	 * @param card
+	 */
+	@Override
+	public void setCreditCard(Optional<CreditCard> card) {
+	}
+	/* @author Jephthia Louis
+	 * @return string of the parameters.
+	 */
+	@Override
+	public String toString() {
+		String card = "*";
+
+		if (creditCard.isPresent())
+			card = creditCard.get().getType().toString() + "*" + creditCard.get().getNumber();
+
+		return getEmail().getAddress() + "*" + getName().getFirstName() + "*" + getName().getLastName() + "*" + card;
+	}
 }
--- ReservationSys/src/group1/hotel/business/DawsonRoom.java
+++ ReservationSys/src/group1/hotel/business/DawsonRoom.java
@@ -3,81 +3,119 @@ package group1.hotel.business;
 import dw317.hotel.business.RoomType;
 import dw317.hotel.business.interfaces.Room;
 
-public class DawsonRoom implements Room
-{
-    private static final long serialVersionUID = 42031768871L;
-    private final int roomNumber;
-    private final RoomType roomType;
-    
-    public DawsonRoom(int roomNumber, RoomType roomType)
-    {
-        this.roomNumber = validateRoomNumber(roomNumber);
-        this.roomType = roomType;
-    }
-    @Override public int compareTo(Room room)
-    {
-        if(this.getRoomNumber() > room.getRoomNumber())
-            return 1;
-        
-        if(this.getRoomNumber() < room.getRoomNumber())
-            return -1;
-        
-        return 0;
-    }
-    @Override public RoomType getRoomType()
-    {
-        return roomType;
-    }
-    @Override public int getRoomNumber()
-    {
-        return roomNumber;
-    }
-    @Override public int getFloor()
-    {
-        return getRoomNumber() / 100;
-    }
-    @Override public int getNumber()
-    {
-        return getRoomNumber() % 100;
-    }
-    @Override public final boolean equals(Object object)
-    {
-        if(object == null)
-            return false;
-        
-        if(this == object)
-            return true;
-        
-        if(object instanceof Room)
-        {
-            Room room = (Room)object;
-            
-            if(getRoomNumber() == room.getRoomNumber())
-                if(getRoomType().name().equals(room.getRoomType().name()))
-                    return true;
-        }
-        return false;
-    }
-    @Override public final int hashCode()
-    {
-        //TODO
-        return 0;
-    }
-    @Override public String toString()
-    {
-        return this.getRoomNumber()+"*"+this.getRoomType().toString();
-    }
-    private int validateRoomNumber(int roomNum) throws IllegalArgumentException
-    {
-        int floor = roomNum / 100;
-        int num = roomNum % 100;
-        
-        if(floor < 1 || floor > 8)
-            throw new IllegalArgumentException("Floor number must be between 1 and 8 inclusive");
-        
-        if(num < 1 || num > 8)
-            throw new IllegalArgumentException("The number must be between 1 and 8 inclusive");
-        
-        return roomNum;
-    }
+/**
+ * @author Jephthia Louis
+ *
+ */
+public class DawsonRoom implements Room {
+	private static final long serialVersionUID = 42031768871L;
+	private final int roomNumber;
+	private final RoomType roomType;
+
+	public DawsonRoom(int roomNumber, RoomType roomType) {
+		this.roomNumber = validateRoomNumber(roomNumber);
+		this.roomType = roomType;
+	}
+	
+	/* @author Jephthia Louis
+	 * @return int
+	 * @param room
+	 */
+	@Override
+	public int compareTo(Room room) {
+		if (this.getRoomNumber() > room.getRoomNumber())
+			return 1;
+
+		if (this.getRoomNumber() < room.getRoomNumber())
+			return -1;
+
+		return 0;
+	}
+	
+	/* @author Jephthia Louis
+	 * @return boolean if the objects are equal or not. Two Room objects 
+	 * are considered equal if they are of the instanceof the
+	 * same class and their room number attributes are equal.
+	 * @param object
+	 */
+	@Override
+	public final boolean equals(Object object) {
+		if (object == null)
+			return false;
+
+		if (this == object)
+			return true;
+
+		if (object instanceof Room) {
+			Room room = (Room) object;
+
+			if (getRoomNumber() == room.getRoomNumber())
+				if (getRoomType().name().equals(room.getRoomType().name()))
+					return true;
+		}
+		return false;
+	}
+	
+	/* @author Jephthia Louis
+	 * @return floor number
+	 */
+	@Override
+	public int getFloor() {
+		return getRoomNumber() / 100;
+	}
+	
+	/* @author Jephthia Louis
+	 * @return room number on floor.
+	 */
+	@Override
+	public int getNumber() {
+		return getRoomNumber() % 100;
+	}
+	
+	/* @author Jephthia Louis
+	 * @return room number
+	 */
+	@Override
+	public int getRoomNumber() {
+		return roomNumber;
+	}
+	
+	/* @author Jephthia Louis
+	 * @return room type
+	 */
+	@Override
+	public RoomType getRoomType() {
+		return roomType;
+	}
+
+	@Override
+	public final int hashCode() {
+		return 0;
+	}
+	
+	/* @author Jephthia Louis
+	 * @return string of roon number and room type
+	 */
+	@Override
+	public String toString() {
+		return this.getRoomNumber() + "*" + this.getRoomType().toString();
+	}
+	/* @author Jephthia Louis
+	 * @return valid room number.
+	 * @param roomNum
+	 * The room number is valid if the room number is between 1 and 8
+	 * and the floor number is between 1 and 8.
+	 */
+	private int validateRoomNumber(int roomNum) throws IllegalArgumentException {
+		int floor = roomNum / 100;
+		int num = roomNum % 100;
+
+		if (floor < 1 || floor > 8)
+			throw new IllegalArgumentException("Floor number must be between 1 and 8 inclusive");
+
+		if (num < 1 || num > 8)
+			throw new IllegalArgumentException("The number must be between 1 and 8 inclusive");
+
+		return roomNum;
+	}
 }
