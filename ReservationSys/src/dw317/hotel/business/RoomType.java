16bedbc1eb03a4890cacfc2cb053ea2ab8c431fd
Andy <andreeaid@users.noreply.github.com>
2016-09-25 19:46:59 -0400

    Add files via upload

--- ReservationSys/src/dw317/hotel/business/RoomType.java
+++ ReservationSys/src/dw317/hotel/business/RoomType.java
@@ -1,11 +1,18 @@
 package dw317.hotel.business;
 
+/** @author Jephthia Louis
+ * 
+ */
+
 public enum RoomType {
 	NORMAL, SUITE, PENTHOUSE;
 	
+	/* @author Jephthia Louis
+	 * @return a lowercase string of the type
+	 */
 	@Override
 	public String toString(){
-		return null;
+		return this.name().toLowerCase();
 	}
 }
 
