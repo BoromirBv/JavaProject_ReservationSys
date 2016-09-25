3c9c8ced9cd0691d1a53ef40aca72d9fea1961a2
Andy <andreeaid@users.noreply.github.com>
2016-09-25 19:46:02 -0400

    Add files via upload

--- ReservationSys/src/group1/hotel/business/DawsonHotelFactory.java
+++ ReservationSys/src/group1/hotel/business/DawsonHotelFactory.java
@@ -9,28 +9,43 @@ import dw317.lib.creditcard.CreditCard;
 
 public enum DawsonHotelFactory implements HotelFactory {
 	DAWSON;
-
+	
+	/* @return the customer instance
+	 * @param firstname, lastname, email
+	 */
 	@Override
 	public Customer getCustomerInstance(String firstName, String lastName, String email) {
 		return new DawsonCustomer(firstName, lastName, email);
 	}
-
+	
+	/* @return credit card
+	 * @param cardtype, number
+	 */
 	@Override
 	public CreditCard getCard(String cardtype, String number) {
 		return CreditCard.getInstance(CreditCard.CardType.valueOf(cardtype.toUpperCase()), number);
 	}
-
+	
+	/* @return the room instance
+	 * @param roomNumber, roomtype
+	 */
 	@Override
 	public Room getRoomInstance(int roomNumber, String roomtype) {
 		return new DawsonRoom(roomNumber, RoomType.valueOf(roomtype.toUpperCase()));
 	}
 
+	/* @return new Reservation
+	 * @param aCustomer, aRoom, inYear, inMonth, inDay, outYear, outMonth, outDay
+	 */
 	@Override
 	public Reservation getReservationInstance(Customer aCustomer, Room aRoom, int inYear, int inMonth, int inDay,
 			int outYear, int outMonth, int outDay) {
 		return new DawsonReservation(aCustomer, aRoom, inYear, inMonth, inDay, outYear, outMonth, outDay);
 	}
-
+	
+	/* @return the reservation instance
+	 * @param toCopy
+	 */
 	@Override
 	public Reservation getReservationInstance(Reservation toCopy) {
 		return  new DawsonReservation(toCopy.getCustomer(), toCopy.getRoom(), toCopy.getCheckInDate().getYear(),
