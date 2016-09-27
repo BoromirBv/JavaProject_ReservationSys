b5ff2b69c97451fcd6413a55481c06d045866544
Jephthia <Jephthia4@hotmail.com>
2016-09-26 21:09:42 -0400

    Delete addressbooksys

--- AddressBookSys/src/addressbook/app/AddressBook.java
+++
@@ -1,9 +0,0 @@
-package addressbook.app;
-import dw317.lib.Address;
-
-public class AddressBook {
-	public static void main(String[] args) {
-		Address myAddress = new Address("3040", "Sherbrooke", "Montreal");
-		System.out.println(myAddress.getAddress());
-	}
-}
